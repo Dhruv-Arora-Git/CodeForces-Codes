56caf834d8bd6b64435f38731afbd8a2fd8fd805
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2022-04-27 22:42:29 +0530

    done

---
+++ 200_B_Drinks.cpp
@@ -0,0 +1,66 @@
+/*
+ ___
+|   \  | |__    _,_           __     __
+| /\ \ | '_ \  | __| | | | |  \ \   / /
+| \/ / | | | | | |   | (_) |   \ \_/ /
+|___/  |_| |_| |_|   \ ___ /    \___/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+#define ll long long int
+#define ld long double
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << " = " << x << '\n'
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+
+// ----------------------------------------------
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll Lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll fastPower(ll base, ll power)
+{
+    ll ans = 1;
+    while (base > 0)
+    {
+        if ((power & 1) == 1)
+            ans *= base;
+        base = base * base;
+        power = power >> 1;
+    }
+    return ans;
+}
+// ----------------------------------------------
+
+const int mod = 1'000'000'007;
+
+void dhruv()
+{
+    ld n;
+    cin >> n;
+
+    ld total = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int a;
+        cin >> a;
+        total += (a / 100.0);
+    }
+
+    cout << ((total / n) * 100) << '\n';
+}
+
+int main()
+{
+    fastio;
+    cout << fixed << setprecision(9);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        // solve function
+        dhruv();
+    }
+
+    return 0;
+}
