3ce7d1236e542d147e5699a89c5fc80b3888a2bb
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2022-12-06 20:18:03 +0530

    Easy greedy

---
+++ A_Hit_the_Lottery.cpp
@@ -0,0 +1,85 @@
+// https://codeforces.com/problemset/problem/996/A
+
+/*
+ ___
+|   \  | |__    _,_           __     __
+| /\ \ | '_ \  | __| | | | |  \ \   / /
+| \/ / | | | | | |   | (_) |   \ \_/ /
+|___/  |_| |_| |_|   \ ___ /    \___/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+#define ll long long int
+#define ld long double
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << " = " << x << '\n'
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+
+// ----------------------------------------------
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll Lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll fastPower(ll base, ll power)
+{
+    ll ans = 1;
+    while (base > 0)
+    {
+        if ((power & 1) == 1)
+            ans *= base;
+        base = base * base;
+        power = power >> 1;
+    }
+    return ans;
+}
+// ----------------------------------------------
+
+const int mod = 1'000'000'007;
+
+void dhruv()
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+
+    while (n >= 100)
+    {
+        n -= 100;
+        ++ans;
+    }
+    while (n >= 20)
+    {
+        n -= 20;
+        ++ans;
+    }
+    while (n >= 10)
+    {
+        n -= 10;
+        ++ans;
+    }
+    while (n >= 5)
+    {
+        n -= 5;
+        ++ans;
+    }
+    while (n >= 1)
+    {
+        n -= 1;
+        ++ans;
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    fastio;
+    // cout<<fixed<<setprecision(9);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        // solve function
+        dhruv();
+    }
+
+    return 0;
+}
