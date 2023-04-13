70f25059448c241002ba22c456838f545691c061
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2023-04-13 23:09:13 +0530

    just find odd and even sum

---
+++ 1807_B_Grab_the_Candies.cpp
@@ -0,0 +1,71 @@
+/*
+ ___
+|   \  | |__    _,_           __     __
+| /\ \ | '_ \  | __| | | | |  \ \   / /
+| \/ / | | | | | |   | (_) |   \ \_/ /
+|___/  |_| |_| |_|   \ ___ /    \___/
+*/
+// https://codeforces.com/problemset/problem/1807/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+#define ll long long int
+#define ld long double
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << " = " << x << '\n'
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+
+// ----------------------------------------------
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll Lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll fastPower(ll base, ll power)
+{
+    ll ans = 1;
+    while (base > 0)
+    {
+        if ((power & 1) == 1)
+            ans *= base;
+        base = base * base;
+        power = power >> 1;
+    }
+    return ans;
+}
+// ----------------------------------------------
+
+const int mod = 1'000'000'007;
+
+void dhruv()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    int evenSum = 0, oddSum = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+        if (arr[i] & 1)
+            oddSum += arr[i];
+        else
+            evenSum += arr[i];
+    }
+    if (evenSum > oddSum)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    fastio;
+    // cout<<fixed<<setprecision(9);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // solve function
+        dhruv();
+    }
+
+    return 0;
+}
