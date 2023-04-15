79420deeef1d791c8b9c4bb9cc173b8916b45bbb
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2023-04-15 12:53:55 +0530

    start from back and check for 0

---
+++ 1729_B_Decode_String.cpp
@@ -0,0 +1,83 @@
+/*
+ ___
+|   \  | |__    _,_           __     __
+| /\ \ | '_ \  | __| | | | |  \ \   / /
+| \/ / | | | | | |   | (_) |   \ \_/ /
+|___/  |_| |_| |_|   \ ___ /    \___/
+*/
+// https://codeforces.com/contest/1729/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+#define ll long long int
+#define ld long double
+#define mp make_pair
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << " = " << x << '\n'
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+
+// ----------------------------------------------
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll Lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll fastPower(ll base, ll power)
+{
+    ll ans = 1;
+    while (base > 0)
+    {
+        if ((power & 1) == 1)
+            ans *= base;
+        base = base * base;
+        power = power >> 1;
+    }
+    return ans;
+}
+// ----------------------------------------------
+
+const int mod = 1'000'000'007;
+
+void dhruv()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    string code = "";
+
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (s[i] == '0')
+        {
+            string t = "00";
+            --i;
+            t[1] = s[i];
+            --i;
+            t[0] = s[i];
+            int num = stoi(t);
+
+            code.push_back(char(num + 'a' - 1));
+        }
+        else
+        {
+            code.push_back(char((s[i] - '0') + 'a' - 1));
+        }
+    }
+    reverse(code.begin(), code.end());
+    cout << code << "\n";
+}
+
+int main()
+{
+    fastio;
+    // cout<<fixed<<setprecision(9);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // solve function
+        dhruv();
+    }
+
+    return 0;
+}
