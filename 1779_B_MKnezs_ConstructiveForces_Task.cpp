bdc0fbf8eda89b726885f9870c1cc9b194c5e2a4
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2023-04-21 23:49:11 +0530

    -1 +1

---
+++ 1779_B_MKnezs_ConstructiveForces_Task.cpp
@@ -0,0 +1,86 @@
+/*
+ ___
+|   \  | |__    _,_           __     __
+| /\ \ | '_ \  | __| | | | |  \ \   / /
+| \/ / | | | | | |   | (_) |   \ \_/ /
+|___/  |_| |_| |_|   \ ___ /    \___/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+#define ll long long int
+#define ld long double
+#define mp make_pair
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define for(i, n, a) for (int i = 0; i < n; i += a)
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << " = " << x << '\n'
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+
+// ----------------------------------------------
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll Lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll fastPower(ll base, ll power)
+{
+    ll ans = 1;
+    while (base > 0)
+    {
+        if ((power & 1) == 1)
+            ans *= base;
+        base = base * base;
+        power = power >> 1;
+    }
+    return ans;
+}
+// ----------------------------------------------
+
+const int mod = 1'000'000'007;
+
+void dhruv()
+{
+    int a;
+    cin >> a;
+    if (a == 3)
+    {
+        cout << "NO\n";
+    }
+    else if (a & 1)
+    {
+        cout << "YES\n";
+
+        int first = ((a - ((a / 2) + 2)) * -1) - 1;
+        int sec = (a - ((a / 2) + 2));
+
+        for (i, a - 1, 2)
+        {
+            cout << sec << " " << first << " ";
+        }
+
+        cout << sec << "\n";
+    }
+    else
+    {
+        cout << "YES\n";
+        for (i, a, 2)
+        {
+            cout << "-1 1 ";
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    fastio;
+    // cout<<fixed<<setprecision(9);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // solve function
+        dhruv();
+    }
+
+    return 0;
+}
